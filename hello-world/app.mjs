0187521f5244b152da8a0b04e74596bcf924f356
RameezSTC <ramakhdoomi@stc.in>
2024-01-09 15:39:21 +0530

    featured branch text

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -15,7 +15,7 @@ export const lambdaHandler = async (event, context) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello world from rameez pipelines and githib actions',
+        message: 'featured push',
       })
     };
 
